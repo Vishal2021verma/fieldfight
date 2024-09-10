ded1a072051b21852e42e441021fc79f0521c221
Vishal2021verma <black2020panther@gmail.com>
2024-09-10 23:02:30 +0530

    style : design game interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,13 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -25,7 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  
   final String title;
 
   @override
@@ -33,46 +33,108 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double topContainerHeight = 0;
+  double bottomContainerheight = 0;
+  Color? topContainerColor;
+  Color? bottomContainerColor;
+  topContainerHit() {
+    if (bottomContainerheight > 1) {
+      setState(() {
+        bottomContainerheight =
+            bottomContainerheight - MediaQuery.sizeOf(context).height * 0.005;
+      });
+    }
+  }
 
-  void _incrementCounter() {
-    setState(() {
-     
-      _counter++;
+  bottomContainerHit() {
+    if (bottomContainerheight <= MediaQuery.sizeOf(context).height) {
+      setState(() {
+        bottomContainerheight =
+            bottomContainerheight + MediaQuery.sizeOf(context).height * 0.005;
+      });
+    }
+
+    print(MediaQuery.sizeOf(context).height);
+    print(bottomContainerheight);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      topContainerHeight = MediaQuery.sizeOf(context).height / 2;
+      bottomContainerheight = MediaQuery.sizeOf(context).height / 2;
+      topContainerColor = const Color(0xff003285);
+      bottomContainerColor = const Color(0xffFF7F3E);
+      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
-      appBar: AppBar(
-       
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-     
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SizedBox(
+        width: MediaQuery.sizeOf(context).width,
+        height: MediaQuery.sizeOf(context).height,
+        child: Stack(
+          children: [
+            InkWell(
+              onTap: topContainerHit,
+              child: Container(
+                width: double.infinity,
+                height: MediaQuery.sizeOf(context).height,
+                decoration: BoxDecoration(color: topContainerColor),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: InkWell(
+                onTap: bottomContainerHit,
+                child: AnimatedContainer(
+                  width: double.infinity,
+                  height: bottomContainerheight,
+                  duration: const Duration(milliseconds: 100),
+                  decoration: BoxDecoration(color: bottomContainerColor),
+                  curve: Curves.easeInOut,
+                ),
+              ),
             ),
+            bottomContainerheight >= MediaQuery.sizeOf(context).height
+                ? const Positioned(
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Center(
+                      child: Text(
+                        "Orange Wins",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w800,
+                            fontSize: 36),
+                      ),
+                    ))
+                : const SizedBox.shrink(),
+            bottomContainerheight <= 1
+                ? const Positioned(
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Center(
+                      child: Text(
+                        "Blue Wins",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w800,
+                            fontSize: 36),
+                      ),
+                    ))
+                : const SizedBox.shrink()
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
