4483d3b7ffa0cd73ec1a9e76e832d0c435f6f7f2
Vishal2021verma <black2020panther@gmail.com>
2024-09-15 12:36:24 +0530

    start button

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:fieldfight/extension/slide_from_left_animation_extension.dart';
 import 'package:fieldfight/extension/slide_from_right_animation_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -55,6 +56,42 @@ class _HomeScreenState extends State<HomeScreen> {
                       fontWeight: FontWeight.w600),
                 ).sildeFromRight()
               ],
+            ),
+
+            Spacer(),
+
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.all(40),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple, width: 5),
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color.fromARGB(122, 0, 0, 0),
+                        spreadRadius: 0,
+                        blurRadius: 10,
+                      ),
+                    ]),
+                child: Shimmer.fromColors(
+                  baseColor: Colors.red,
+                  highlightColor: Colors.yellow,
+                  child: const Text(
+                    "Start",
+                    style: TextStyle(
+                        fontFamily: "RussoOne",
+                        color: Color(0xffFF7F3E),
+                        fontSize: 40,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.15,
             )
           ],
         ),
